49193d2f17dc19fd54c57b801e7ef18ae55a253a
Salvador-gr <146394181+Sal-GR@users.noreply.github.com>
2026-09-07 17:07:42 -0500

    adding wiring for database

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+from models import db, Purchases, DailyLogs
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+  db.create_all()
 
 @app.route('/')
 def home():
@@ -8,4 +16,3 @@ def home():
 
 if __name__ == '__main__':
   app.run(debug=True)
-  
